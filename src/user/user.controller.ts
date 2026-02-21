ff1175eb515953f9e2b98ebfd3c5a963eb652402
mfurqanabbas20 <mfurqanabbas20@gmail.com>
2026-02-21 15:22:18 +0500

    feat: create user auth controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('user') // decorator
+export class UserController {
+    @Get()
+    getUser(){
+        return 'User data fetched successfully';
+    }
+}
